3a5c872dce93eac24040d3dd0b38956a44fcd054
raj_khan <meherullah97@gmail.com>
2020-12-03 11:26:22 +0600

    auth: laravel-breeze installed and configured

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', 'HomeController@index')->name('index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
+require __DIR__.'/auth.php';
